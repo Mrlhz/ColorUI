2aa96aa192a1cfbed4a76819151c8df4b7d107d8
mrlhz <mr__lhz@163.com>
2020-03-26 00:00:03 +0800

    add util method

---
+++ utils/dom/event.js
@@ -0,0 +1,15 @@
+
+export function stopPropagation (event) {
+  event.stopPropagation()
+}
+
+export function preventDefault (event, isStopPropagation) {
+  /* istanbul ignore else */
+  if (typeof event.cancelable !== 'boolean' || event.cancelable) {
+    event.preventDefault()
+  }
+
+  if (isStopPropagation) {
+    stopPropagation(event)
+  }
+}
